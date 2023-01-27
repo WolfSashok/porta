519276eea36107cedb8a78469b511e7e617f8b18
root <root@porta.europe-central2-a.c.original-list-372101.internal>
2023-01-27 14:34:49 +0200

    GeoIP added

--- www/action.php
+++ www/action.php
@@ -1,18 +1,17 @@
 <?php
 
-//Get users IP address
+//Funct - Get users IP address
 function getUserIpAddr() {
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-$ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-$ip = $_SERVER['REMOTE_ADDR'];
-}
-return $ip;
-}
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+        return $ip;
+    }
 $ip = getUserIpAddr();
-
 
 //Geo IP
 require_once 'SxGeo.php';
@@ -22,39 +21,20 @@ $country_code = $city['country']['iso'];
 $city_name_en = $city['city']['name_en'];
 $lat = $city['city']['lat'];
 $lon = $city['city']['lon'];
-//Geo IP
 
+//Work with a text. Exec bash script with algorithm
 if(isset($_POST["text"]) && isset($_POST["language"])) 
 {
     $text = htmlentities($_POST["text"]);
     $text = str_replace("\n", "", $text);
 
-//echo $text;
-
     $result = shell_exec("bash algoritm.sh '".$text."'");
-
-//    $output ="
-//    <html>
-//    <head>
-//    <title>Повторяем текст</title>
-//    </head>
-//    <body>
-//    You wrote: $text<br />
-//    <ul>";
-
 
     echo $result;
 
-//    foreach (count_chars($text, 1) as $i => $val) {
-//        echo "\"" , chr($i) , "\" встречается в строке $val раз(а).\n";
-//        echo "<br>";
-//    }
-
-//    echo $count";
-//    echo $output;
 }
 else
-{   
+{
     echo "Введенные данные некорректны<br>";
 }
 
